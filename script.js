78c240a76bf0e832d303b78dc92b47bd20b018b5
icheff <94357762+icheff@users.noreply.github.com>
2023-10-19 12:20:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,65 @@
-function addTemplate(url, from, to, position) {
-  fetch(url)
-    .then(response => response.text())
-    .then(html => {
-      const tempElement = document.createElement('div');
-      tempElement.innerHTML = html;
-      const sectionElement = tempElement.querySelector(from);
+// Función asíncrona para agregar plantillas de manera más eficiente
+async function addTemplate(url, from, to, position) {
+    try {
+        const response = await fetch(url);
+        const html = await response.text();
 
-      if (!sectionElement) {
-        console.error(`Element not found with selector: ${from}`);
-        return;
-      }
+        // Comprobar si el contenido HTML está vacío
+        if (!html.trim()) {
+            console.warn('External HTML content is empty:', url);
+            return;
+        }
 
-      const contentContainer = document.querySelector(to);
-      if (!contentContainer) {
-        console.error(`Element not found with selector: ${to}`);
-        return;
-      }
+        const tempElement = document.createElement('div');
+        tempElement.innerHTML = html;
+        const sectionElement = tempElement.querySelector(from);
 
-      if (position === 'top') {
-        contentContainer.prepend(sectionElement.cloneNode(true));
-      } else if (position === 'bottom') {
-        contentContainer.appendChild(sectionElement.cloneNode(true));
-      } else {
-        console.error('Invalid position parameter. Use "top" or "bottom".');
-      }
-    })
-    .catch(error => {
-      console.error('Error loading external HTML file:', error);
-    });
+        if (!sectionElement) {
+            console.error(`Element not found with selector: ${from}`);
+            return;
+        }
+
+        const contentContainer = document.querySelector(to);
+        if (!contentContainer) {
+            console.error(`Element not found with selector: ${to}`);
+            return;
+        }
+
+        switch (position) {
+            case 'top':
+                contentContainer.prepend(sectionElement.cloneNode(true));
+                break;
+            case 'bottom':
+                contentContainer.appendChild(sectionElement.cloneNode(true));
+                break;
+            default:
+                console.error('Invalid position parameter. Use "top" or "bottom".');
+        }
+    } catch (error) {
+        console.error('Error loading external HTML file:', error);
+    }
 }
 
-var parts = window.location.pathname.split('/');
-// Remove the last element if it's empty or specific to a sub-page
-if (parts[parts.length - 1] !== 'argent' && parts[parts.length - 1] !== '') {
-  parts.pop();
+// Función para obtener la base del path
+function getBasePath() {
+    const parts = window.location.pathname.split('/');
+    if (parts[parts.length - 1] !== 'argent' && parts[parts.length - 1] !== '') {
+        parts.pop();
+    }
+    const index = parts.indexOf('argent');
+    if (index !== -1) {
+        return window.location.origin + parts.slice(0, index + 1).join('/');
+    }
+    return window.location.origin + parts.join('/');
 }
-// Find the index of 'argent' in the array to set as the base directory
-var index = parts.indexOf('argent');
-if (index !== -1) {
-  parts = parts.slice(0, index + 1);
-}
 
-var basePath = window.location.origin + parts.join('/');
-
-// Now your basePath should be correct for both root and sub-folders
-
-
-// Add content to the beginning of the container
+const basePath = getBasePath();
 addTemplate(`${basePath}/header/index.html`, '.header', '.page', 'top');
-// Add content to the end of the container
 addTemplate(`${basePath}/footer/index.html`, '.footer', '.page', 'bottom');
-// Add content to the end of the container
 addTemplate(`${basePath}/sidebar/index.html`, '.sidebar', '.page', 'bottom');
 
-
-function open_right_sidebar() {				
-  document.getElementsByClassName("sidebar")[0].style.right = "0";
-  document.getElementsByClassName("sidebar")[0].style.transition = "0.3s";
-  
+function open_right_sidebar() {
+    const sidebar = document.getElementsByClassName("sidebar")[0];
+    sidebar.style.right = "0";
+    sidebar.style.transition = "0.3s";
 }
-
